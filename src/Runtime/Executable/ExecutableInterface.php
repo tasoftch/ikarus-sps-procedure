bbe98a2c0c1bc53ddf8e47ced4cdea53130d9a56
Th. Abplanalp <info@tasoft.ch>
2021-02-08 16:55:33 +0100

    REMAKE

---
+++ src/Runtime/Executable/ExecutableInterface.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2019, TASoft Applications
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ *  Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace Ikarus\SPS\Procedure\Runtime\Executable;
+
+
+use Ikarus\SPS\Register\MemoryRegisterInterface;
+
+/**
+ * This interface describes the executable's method signature.
+ * You must use closures because Ikarus SPS will extract the code into a compiled version.
+ * The closure will not be executed in its class context!
+ *
+ * @package Ikarus\SPS\Dev\Node
+ */
+interface ExecutableInterface
+{
+	/**
+	 * This method is responsible that every node using the executable's component is performed.
+	 *
+	 * @param MemoryRegisterInterface $memoryRegister
+	 * @param array $nodeData
+	 * @param array $inputs
+	 * @param array $outputs
+	 */
+	public function __invoke(array $nodeData, InputRegister $inputs, OutputRegister $outputs, MemoryRegisterInterface $memoryRegister);
+}
